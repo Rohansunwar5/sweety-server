7ac2115a87852fbaf9acf5349def5221db8ac3ac
Rohan Sunwar <rohansunwar5@gmail.com>
2025-06-25 17:20:45 +0530

    fixed update profile

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express';
 import { asyncHandler } from '../utils/asynchandler';
-import { genericLogin, googleSignIn, profile, signup } from '../controllers/auth.controller';
+import { genericLogin, googleSignIn, profile, signup, updateProfile } from '../controllers/auth.controller';
 import { loginValidator,signupValidator} from '../middlewares/validators/auth.validator';
 import isLoggedIn from '../middlewares/isLoggedIn.middleware';
-import { updateProduct } from '../controllers/product.controllers';
 
 const authRouter = Router();
 
 authRouter.post('/login', loginValidator, asyncHandler(genericLogin));
 authRouter.post('/signup', signupValidator, asyncHandler(signup));
 authRouter.get('/profile', isLoggedIn, asyncHandler(profile));
-authRouter.patch('/profile', isLoggedIn, asyncHandler(updateProduct));
+authRouter.patch('/profile', isLoggedIn, asyncHandler(updateProfile));
 authRouter.post('/google', asyncHandler(googleSignIn));
 
 export default authRouter;
